7c95f868854ef602849a1b447b599504eb9c4e7c
Ash <valleyknight@protonmail.com>
2020-05-28 12:29:27 -0400

    move big yt/tw checking function to separate file, then mod/use it into main and call it for normal messages

--- src/commands/date.rs
+++ src/commands/date.rs
@@ -10,7 +10,7 @@ use serenity::{
 fn date(ctx: &mut Context, msg: &Message) -> CommandResult {
     let date = DateTuple::today().to_readable_string(); // dd mmm yyyy
     let time = TimeTuple::now().to_hhmm_string(); // 00:00, 24-hour time
-    let date = time + &" | ".to_string() + &date;
+    let date = time + &" | ".to_string() + &date; // example output: 14:47 | 28 May 2020
 
     if let Err(why) = msg.channel_id.say(&ctx.http, date) {
         println!("Error sending message: {:?}", why);
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,10 @@ use std::{
     sync::Arc,
 };
 
+// Load yt_tw_check function
+mod yt_tw_check;
+use yt_tw_check::yt_tw_check;
+
 // Load and use commands from src/commands/
 mod commands;
 use commands::{
@@ -176,58 +180,7 @@ fn main() {
                 }
             })
             .normal_message(|ctx, msg| {
-                // If "youtube.com" is found, react with disappointed and message a pretty embed about invidio.
-                // Do the same for "twitter.com", except the embed pertains to Nitter.
-                let sent_message = &msg.content;
-                if sent_message.contains("youtube.com") {
-                    info!("YouTube link was found!");
-                    msg.react(&ctx.http, '😞');
-                    let message = msg.channel_id.send_message(&ctx.http, |m| {
-                        m.embed(|e| {
-                            e.title("`rsPhate`");
-                            e.description("YouTube link discovered! Please link to invidio instead. Why:");
-                            // false = not inline
-                            e.fields(vec![
-                                ("FOSS", "Invidio is a [FOSS](https://github.com/omarroth/invidious) frontend to YouTube.", false),
-                                ("Proxy", "Invidio has the ability to proxy YouTube videos through their servers, allowing you to watch videos without ever touching Google's servers. Add `&local=1` to an invidio URL to proxy.", false),
-                                ("Lightweight", "The entire site is very small, and works without JavaScript or XHR.", false),
-                                ("Self-Hostable", "You can self-host your own instance if you really wanted to. And if you don't trust the [main instance](https://invidio.us) there are a [variety of other instances](https://github.com/omarroth/invidious/wiki/Invidious-Instances) to choose from.", false),
-                                ("I'm interested, how do I convert URLs?", "Replace `youtube.com` with `invidio.us`. For `youtu.be` URLs, take the string after the last slash and append it to `https://invidio.us/watch?v=`.", false),
-                            ]);
-                            e
-                        });
-                        m
-                    });
-
-                    if let Err(why) = message {
-                        error!("Error sending message: {:?}", why);
-                    }
-                }
-                if sent_message.contains("twitter.com") {
-                    info!("Twitter link was found!");
-                    msg.react(&ctx.http, '😞');
-                    let message = msg.channel_id.send_message(&ctx.http, |m| {
-                        m.embed(|e| {
-                            e.title("`rsPhate`");
-                            e.description("Twitter link discovered! Please link to Nitter instead. Why:");
-                            // false = not inline
-                            e.fields(vec![
-                                ("FOSS", "Nitter is a [FOSS](https://github.com/zedeus/nitter) frontend to Twitter inspired by invidio.", false),
-                                ("Proxy", "Everything is proxied through Nitter, Twitter won't even know your IP, much less be able to fingerprint or track you.", false),
-                                ("API", "Contains an unoffical twitter api without any keys required or any rate limits enforced.", false),
-                                ("Lightweight", "Pages are very small, and work without JS and XHR.", false),
-                                ("Self-Hostable", "As with invidio, it is self-hostable and there are plenty of other [instances](https://github.com/zedeus/nitter/wiki/Instances) to choose from.", false),
-                                ("I'm interested, how do I convert URLs?", "Replace `twitter.com` with `nitter.net`.", false),
-                            ]);
-                            e
-                        });
-                        m
-                    });
-
-                    if let Err(why) = message {
-                        error!("Error sending message: {:?}", why);
-                    }
-                }
+                yt_tw_check(ctx, msg);
             })
             // Set a function that's called whenever a command's execution didn't complete for one
             // reason or another. For example, when a user has exceeded a rate-limit or a command
