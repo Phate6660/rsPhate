8da58e47a6f24b0873f14272623e5442081b4d91
Ash <valleyknight@protonmail.com>
2020-05-28 10:54:14 -0400

    add comments

--- src/commands/date.rs
+++ src/commands/date.rs
@@ -6,10 +6,10 @@ use serenity::{
 };
 
 #[command]
-#[description = "Display the date in format: `06:30 AM | Mon 25, May of 2020`."]
+#[description = "Display the date in format: `14:47 | 28 May 2020`."]
 fn date(ctx: &mut Context, msg: &Message) -> CommandResult {
-    let date = DateTuple::today().to_readable_string();
-    let time = TimeTuple::now().to_hhmm_string();
+    let date = DateTuple::today().to_readable_string(); // dd mmm yyyy
+    let time = TimeTuple::now().to_hhmm_string(); // 00:00, 24-hour time
     let date = time + &" | ".to_string() + &date;
 
     if let Err(why) = msg.channel_id.say(&ctx.http, date) {
