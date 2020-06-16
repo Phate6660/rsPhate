57ef5d259e0db171847cdcbade53dc04208602e1
Ash <valleyknight@protonmail.com>
2020-06-16 11:04:42 -0400

    add codeberg support for git command

--- src/commands/git.rs
+++ src/commands/git.rs
@@ -9,8 +9,9 @@ use serenity::{
 #[command]
 #[description = "Bot will parse the input and output the correct full link to the repo."]
 #[usage = "site user/repo"]
-#[example = "hub Phate6660/rsPhate"]
-#[example = "lab ArcticTheRogue/asgl"]
+#[example = "github rsfetch/rsfetch"]
+#[example = "gitlab ArcticTheRogue/asgl"]
+#[example = "codeberg Phate6660/rsPhate"]
 #[num_args(2)]
 fn git(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
     let site = args.single::<String>()?;
@@ -18,23 +19,27 @@ fn git(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
     let match_site = site.as_str();
 
     // Log what was supplied
-    info!("site: Git{}, owner/repo: {}", site, repo);
+    info!("site: {}, owner/repo: {}", site, repo);
 
     // Match for site to create message.
     let message: String = match match_site {
-        "hub" => MessageBuilder::new()
+        "github" => MessageBuilder::new()
             .push("https://github.com/")
             .push(repo)
             .build(),
-        "lab" => MessageBuilder::new()
+        "gitlab" => MessageBuilder::new()
             .push("https://gitlab.com/")
+            .push(repo)
+            .build(),
+        "codeberg" => MessageBuilder::new()
+            .push("https://codeberg.org/")
             .push(repo)
             .build(),
         _ => "Could not generate a full link, please try again.".to_string(),
     };
 
     if let Err(why) = msg.channel_id.say(&ctx.http, &message) {
-        error!("Could not push full Git* link because: {}", why);
+        error!("Could not push full Git repo link because: {}", why);
     }
 
     Ok(())
